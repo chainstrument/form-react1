e13d1e015809ebf45991f983d63d74f3483dcf11
Chainstrument <minitutoriel@gmail.com>
2024-09-16 21:48:06 +0200

    add handle change input

--- app/components/Form.tsx
+++ app/components/Form.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
 
 function Form() {
+
+    const [name, setName] = useState('')
+
+    const handleChange = (e) => {
+         setName(e.target.value)
+    }
+
   return (
     <div>
         bonjour:
-        <input type="text" className=''/>
+        <input type="text" className='' onChange={handleChange} value={name} />
+        {name}
     </div>
   )
 }
